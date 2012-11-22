eb20fc6fe0525ed7bb4eddb2b52dc2bc339d6749
vitalets <noginsk@rambler.ru>
2012-11-22 14:18:45 +0400

    shown event described only in container

--- src/containers/editable-container.js
+++ src/containers/editable-container.js
@@ -62,6 +62,11 @@ Applied as jQuery method.
                     
                     @event shown 
                     @param {Object} event event object
+                    @example
+                    $('#username').on('shown', function() {
+                         var $tip = $(this).data('editableContainer').tip();
+                         $tip.find('input').val('overwriting value of input..');
+                    });                     
                     **/                      
                     this.$element.triggerHandler('shown');
                 }, this) 
