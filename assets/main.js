046de3a3b8a4061f6eac5156f1403b83eaa9922f
vitalets <noginsk@rambler.ru>
2013-01-14 16:12:49 +0400

    fix scrollspy bug in bs2.2.2

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,14 @@
 $(function(){
-    prettyPrint();
+   prettyPrint();
+    
+   if($('#subnav').length) {
+       $('body').scrollspy({
+           target: '#subnav',
+           offset: 100
+       });   
+       
+       $('#subnav a').mousedown(function(e){
+           $('body').scrollspy('refresh');
+       });
+   }
 });
