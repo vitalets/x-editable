5ba3004175199d6a86b7fcc648694125d0881f9d
vitalets <noginsk@rambler.ru>
2013-05-07 00:16:39 +0400

    cdn ready

--- assets/main.js
+++ assets/main.js
@@ -11,4 +11,14 @@ $(function(){
            $('body').scrollspy('refresh');
        });
    }
+   
+   $('.cdn input[type="radio"]').click(function(){
+       var css = '<link href="//cdnjs.cloudflare.com/ajax/libs/x-editable/' + window.version+'/'+$(this).data('css')+'">\n',
+           js = '<script src="//cdnjs.cloudflare.com/ajax/libs/x-editable/' + window.version+'/'+$(this).data('js')+'"></script>';
+          
+       $('.cdn pre').text(css+js);
+       prettyPrint(); 
+   });
+   
+   $('.cdn input[type="radio"]').eq(0).click();
 });
