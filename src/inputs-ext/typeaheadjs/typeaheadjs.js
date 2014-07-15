159007f7275f79631129f5dda0a158efce7cc7ca
taivo <tai.a.vo@gmail.com>
2014-07-14 18:10:38 -0700

    make typeaheadjs extension compat with current twitter typeahead
    Current twitter typeahead is not instantiated with 1 single object. Rather it's this way:
    
    jQuery#typeahead(options, [*datasets])
    
    See https://github.com/twitter/typeahead.js/blob/master/doc/jquery_typeahead.md#usage

--- src/inputs-ext/typeaheadjs/typeaheadjs.js
+++ src/inputs-ext/typeaheadjs/typeaheadjs.js
@@ -13,7 +13,7 @@ It is mainly replacement of typeahead in Bootstrap 3.
 $(function(){
     $('#country').editable({
         value: 'ru',
-        typeahead: {
+        typeahead: [{minLength: 2}, {
             name: 'country',
             local: [
                 {value: 'ru', tokens: ['Russia']}, 
@@ -23,7 +23,7 @@ $(function(){
             template: function(item) {
                 return item.tokens[0] + ' (' + item.value + ')'; 
             } 
-        }
+        }]
     });
 });
 </script>
@@ -42,7 +42,11 @@ $(function(){
             this.renderClear();
             this.setClass();
             this.setAttr('placeholder');
-            this.$input.typeahead(this.options.typeahead);
+            if($.isArray(this.options.typeahead)){
+                this.$input.typeahead.apply(this.$input, this.options.typeahead);
+            } else{
+                this.$input.typeahead(this.options.typeahead);
+            }
             
             // copy `input-sm | input-lg` classes to placeholder input
             if($.fn.editableform.engine === 'bs3') {
